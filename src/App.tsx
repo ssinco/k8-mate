0995008fa283539f0caa62b730e5ed5cd8694657
rto24 <richardto180@gmail.com>
2024-09-07 16:44:52 -0400

    configured tailwind and added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import './App.css'
-
+import './index.css'
+import { Hero } from './components/template/pocket/Hero'
+import { Header } from './components/template/pocket/Header'
+import { PrimaryFeatures } from './components/template/pocket/PrimaryFeatures'
+import { Footer } from './components/template/pocket/Footer'
 function App() {
 
   return (
     <>
-     <div>SuperKuber Landing</div>
+      <Header />
+      <Hero />
+      <PrimaryFeatures />
+      <Footer />
     </>
   )
 }
