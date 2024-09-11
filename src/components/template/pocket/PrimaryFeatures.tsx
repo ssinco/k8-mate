e3f9cf53d579d3b5d67b80cb1d3d93ff4de0e965
rto24 <richardto180@gmail.com>
2024-09-11 11:35:40 -0400

    finalized gifs

--- src/components/template/pocket/PrimaryFeatures.tsx
+++ src/components/template/pocket/PrimaryFeatures.tsx
@@ -34,8 +34,9 @@ import { Container } from './Container'
 // import testImg1 from './images/logos/forbes.svg'
 // import testImg2 from './images/logos/cbs.svg'
 import testImg3 from './images/logos/cnn.svg'
-import DashboardGif from '../../../assets/gif/dashboard-showcase-ezgif.com-optimize.gif'
-import AlertsGif from '../../../assets/gif/alerts-showcase-ezgif.com-optimize.gif'
+import DashboardGif from '../../../assets/gif/dashboard-showcase.gif'
+import AlertsGif from '../../../assets/gif/alerts-showcase.gif'
+import TreeGif from '../../../assets/gif/tree-showcase.gif'
 const MotionAppScreenHeader = motion(AppScreen.Header)
 const MotionAppScreenBody = motion(AppScreen.Body)
 
@@ -50,7 +51,6 @@ const features = [
     description:
       'Our intuitive dashboards offer a comprehensive view of your cluster metrics, providing real-time insights and detailed analytics to help optimize performance and decision-making.',
     icon: DeviceUserIcon,
-    // screen: InviteScreen,
     image: DashboardGif,
   },
   {
@@ -58,7 +58,6 @@ const features = [
     description:
       'We provide real-time alerts based on key cluster metrics, ensuring you stay informed about performance, resource usage, and potential issues in your infrastructure.',
     icon: DeviceNotificationIcon,
-    // screen: StocksScreen,
     image: AlertsGif,
   },
   {
@@ -66,28 +65,12 @@ const features = [
     description:
       'Our Kubernetes cluster visualization feature offers a clear, interactive view of your infrastructure, making it easy to track relationships between nodes, pods, and services in real-time.',
     icon: DeviceTouchIcon,
-    // screen: InvestScreen,
-    image: testImg3,
+    image: TreeGif,
   },
 ]
 
 function DeviceUserIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
-    // <svg viewBox="0 0 32 32" aria-hidden="true" {...props}>
-    //   <circle cx={16} cy={16} r={16} fill="#A3A3A3" fillOpacity={0.2} />
-    //   <path
-    //     fillRule="evenodd"
-    //     clipRule="evenodd"
-    //     d="M16 23a3 3 0 100-6 3 3 0 000 6zm-1 2a4 4 0 00-4 4v1a2 2 0 002 2h6a2 2 0 002-2v-1a4 4 0 00-4-4h-2z"
-    //     fill="#737373"
-    //   />
-    //   <path
-    //     fillRule="evenodd"
-    //     clipRule="evenodd"
-    //     d="M5 4a4 4 0 014-4h14a4 4 0 014 4v24a4.002 4.002 0 01-3.01 3.877c-.535.136-.99-.325-.99-.877s.474-.98.959-1.244A2 2 0 0025 28V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9a2 2 0 00-2 2v24a2 2 0 001.041 1.756C8.525 30.02 9 30.448 9 31s-.455 1.013-.99.877A4.002 4.002 0 015 28V4z"
-    //     fill="#A3A3A3"
-    //   />
-    // </svg>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="size-6" {...props}>
       <path 
         strokeLinecap="round" 
@@ -112,42 +95,8 @@ function DeviceNotificationIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 }
 
 function DeviceTouchIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
-  // let id = useId()
 
   return (
-    // <svg viewBox="0 0 32 32" fill="none" aria-hidden="true" {...props}>
-    //   <defs>
-    //     <linearGradient
-    //       id={`${id}-gradient`}
-    //       x1={14}
-    //       y1={14.5}
-    //       x2={7}
-    //       y2={17}
-    //       gradientUnits="userSpaceOnUse"
-    //     >
-    //       <stop stopColor="#737373" />
-    //       <stop offset={1} stopColor="#D4D4D4" stopOpacity={0} />
-    //     </linearGradient>
-    //   </defs>
-    //   <circle cx={16} cy={16} r={16} fill="#A3A3A3" fillOpacity={0.2} />
-    //   <path
-    //     fillRule="evenodd"
-    //     clipRule="evenodd"
-    //     d="M5 4a4 4 0 014-4h14a4 4 0 014 4v13h-2V4a2 2 0 00-2-2h-1.382a1 1 0 00-.894.553l-.448.894a1 1 0 01-.894.553h-6.764a1 1 0 01-.894-.553l-.448-.894A1 1 0 0010.382 2H9a2 2 0 00-2 2v24a2 2 0 002 2h4v2H9a4 4 0 01-4-4V4z"
-    //     fill="#A3A3A3"
-    //   />
-    //   <path
-    //     d="M7 22c0-4.694 3.5-8 8-8"
-    //     stroke={`url(#${id}-gradient)`}
-    //     strokeWidth={2}
-    //     strokeLinecap="round"
-    //     strokeLinejoin="round"
-    //   />
-    //   <path
-    //     d="M21 20l.217-5.513a1.431 1.431 0 00-2.85-.226L17.5 21.5l-1.51-1.51a2.107 2.107 0 00-2.98 0 .024.024 0 00-.005.024l3.083 9.25A4 4 0 0019.883 32H25a4 4 0 004-4v-5a3 3 0 00-3-3h-5z"
-    //     fill="#A3A3A3"
-    //   />
-    // </svg>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1" stroke="currentColor" className="size-6" {...props}>
       <path 
         strokeLinecap="round" 
@@ -160,51 +109,51 @@ function DeviceTouchIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   )
 }
 
-const headerAnimation: Variants = {
-  initial: { opacity: 0, transition: { duration: 0.3 } },
-  animate: { opacity: 1, transition: { duration: 0.3, delay: 0.3 } },
-  exit: { opacity: 0, transition: { duration: 0.3 } },
-}
+// const headerAnimation: Variants = {
+//   initial: { opacity: 0, transition: { duration: 0.3 } },
+//   animate: { opacity: 1, transition: { duration: 0.3, delay: 0.3 } },
+//   exit: { opacity: 0, transition: { duration: 0.3 } },
+// }
 
-const maxZIndex = 2147483647
+// const maxZIndex = 2147483647
 
-const bodyVariantBackwards: Variant = {
-  opacity: 0.4,
-  scale: 0.8,
-  zIndex: 0,
-  filter: 'blur(4px)',
-  transition: { duration: 0.4 },
-}
+// const bodyVariantBackwards: Variant = {
+//   opacity: 0.4,
+//   scale: 0.8,
+//   zIndex: 0,
+//   filter: 'blur(4px)',
+//   transition: { duration: 0.4 },
+// }
 
-const bodyVariantForwards: Variant = (custom: CustomAnimationProps) => ({
-  y: '100%',
-  zIndex: maxZIndex - custom.changeCount,
-  transition: { duration: 0.4 },
-})
+// const bodyVariantForwards: Variant = (custom: CustomAnimationProps) => ({
+//   y: '100%',
+//   zIndex: maxZIndex - custom.changeCount,
+//   transition: { duration: 0.4 },
+// })
 
-const bodyAnimation: MotionProps = {
-  initial: 'initial',
-  animate: 'animate',
-  exit: 'exit',
-  variants: {
-    initial: (custom: CustomAnimationProps, ...props) =>
-      custom.isForwards
-        ? bodyVariantForwards(custom, ...props)
-        : bodyVariantBackwards,
-    animate: (custom: CustomAnimationProps) => ({
-      y: '0%',
-      opacity: 1,
-      scale: 1,
-      zIndex: maxZIndex / 2 - custom.changeCount,
-      filter: 'blur(0px)',
-      transition: { duration: 0.4 },
-    }),
-    exit: (custom: CustomAnimationProps, ...props) =>
-      custom.isForwards
-        ? bodyVariantBackwards
-        : bodyVariantForwards(custom, ...props),
-  },
-}
+// const bodyAnimation: MotionProps = {
+//   initial: 'initial',
+//   animate: 'animate',
+//   exit: 'exit',
+//   variants: {
+//     initial: (custom: CustomAnimationProps, ...props) =>
+//       custom.isForwards
+//         ? bodyVariantForwards(custom, ...props)
+//         : bodyVariantBackwards,
+//     animate: (custom: CustomAnimationProps) => ({
+//       y: '0%',
+//       opacity: 1,
+//       scale: 1,
+//       zIndex: maxZIndex / 2 - custom.changeCount,
+//       filter: 'blur(0px)',
+//       transition: { duration: 0.4 },
+//     }),
+//     exit: (custom: CustomAnimationProps, ...props) =>
+//       custom.isForwards
+//         ? bodyVariantBackwards
+//         : bodyVariantForwards(custom, ...props),
+//   },
+// }
 
 function usePrevious<T>(value: T) {
   let ref = useRef<T>()
