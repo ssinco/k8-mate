14b586ba103aa39c33b63cdb20c8e474ec964b2d
User Name <ssinco90@gmail.com>
2024-09-12 11:24:32 -0700

    etc

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/k8-mate',
   plugins: [react()],
   server: {
     host: true, // Allow external access
